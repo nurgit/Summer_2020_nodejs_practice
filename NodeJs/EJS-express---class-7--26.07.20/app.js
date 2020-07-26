6282c238046115f1e44e42c7e3152b3320d7052a
Nur Mohammad <47762059+nurgit@users.noreply.github.com>
2020-07-26 17:14:41 +0600

    EJS-express---class-7

---
+++ NodeJs/EJS-express---class-7--26.07.20/app.js
@@ -0,0 +1,20 @@
+//Declaration
+var express=requre('express');
+var app =express();
+
+//ROUTTIng
+app.get('/'(req,res)=>{
+  red.send('abc.js')
+
+})
+
+app.get('home'(req,res)=>{
+  red.send('hellow')
+
+})
+
+
+app.get(''(req,res)=>{
+  red.send('abc.js')
+
+})
